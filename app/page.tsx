ac9cce016ba3a190c2395b17d163abf5dacb62b5
Charlse Ledesma <realgoodslime@gmail.com>
2024-10-22 19:31:33 +0800

    Edit made in dev branch

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       </div>
       <div className="w-full lg:w-1/2 xl:w-1/2 p-6 text-gray-800">
         <h3 className="text-3xl font-bold leading-tight mb-4">Welcome to my website</h3>
+        <h5 className="text-3xl font-bold leading-tight mb-4">HALO</h5>
         <p className="text-lg leading-relaxed mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Learn More</button>
       </div>
